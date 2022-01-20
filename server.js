3463702ce1f6c41b52a9451fad329178e4086679
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-01-19 21:19:07 -0500

    front end set up

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -9,9 +11,24 @@ app.use(express.static('public'));
 
 const { notes } = require('./Develop/db/db.json');
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('/notes',(req,res)=>{
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
+app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
