b7ad5f2e2f6cfca0f8e65b1db489d2618c2dff96
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-01-22 21:45:47 -0500

    add new object in db

--- server.js
+++ server.js
@@ -21,35 +21,36 @@ app.get('/notes',(req,res)=>{
   res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 //-------------------------------------------------------------------------------------
-
 app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname,'./Develop/db/db.json'))
-  
-});
+   console.log(req.query)
+ 
+   res.sendFile(path.join(__dirname,'./Develop/db/db.json'))
+ });
+ 
 
 app.post('/api/notes', (req, res) => {
-const note= JSON.parse(fs.readFileSync('./Develop/db/db.json'));
-const newnote= res.body;
-note.push(newnote);
-createnewnotes(note);
-res.json(note);
- });
-function createnewnotes(note){
-  const notenew=note;
-  fs.writeFileSync('./Develop/db/db.json',JSON.stringify(notenew));
-}
+const newNotes=req.body;
+console.log("\n\nPOST request - New Note : " + JSON.stringify(newNotes));
+newNotes.id='1';
+newNotes.id++;
+
+const data = JSON.parse(fs.readFileSync('./Develop/db/db.json', "utf8"));
+console.log("\nGET request - Returning notes data: " + JSON.stringify(data));
+
+data.push(newNotes);
+
+fs.writeFileSync('./Develop/db/db.json', JSON.stringify(data));
+
+res.json(data);
+})
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
+
 
 
 //http://localhost:3000/api/notes
-
-
-
-
-
-
 
 //http://localhost:3000/api/db
